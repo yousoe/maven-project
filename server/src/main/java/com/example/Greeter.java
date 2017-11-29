4b2dc3bfe07a5d451ecc73f5028ad62db96583a3
root <root@localhost.localdomain>
2017-11-29 01:21:13 -0600

    fixed checkstyle warnings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -10,10 +10,9 @@ public class Greeter {
    */
   public Greeter() {
 
-  }
-
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+}
+ 
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
